d55c3a5859aaf7f41eb7db3d0370c11f82065c7b
Alberto Ielpo <alberto.ielpo@gmail.com>
2025-11-05 21:13:37 +0100

    Update: hashmap implementation pt2

--- utils/hmap.c
+++ utils/hmap.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 // .h
 /**
@@ -22,23 +23,46 @@ typedef struct
 {
     char *key;
     void *value;
-    HEType type;
+    HEType type;         // 4 bytes
+    uint32_t value_size; // value_size size: 1 or >1 in case of pointer to an array.
+                         // With type HE_TYPE_STR is not mandatory to indicate the size because is \0 terminated
+                         // Is uint32_t (4 bytes) to fit in the padding
 } HEntry;
 
 typedef struct
 {
-    HEntry **entries; // entryies pointer array
+    HEntry **entries; // entries pointer array
     size_t len;
     size_t capacity;
 } HMap;
 
+// TODO: doc
 HMap *hmap_create(size_t capacity);
-void hentry_destroy(HEntry *entry);
+
+// TODO: doc
 void hmap_destroy(HMap *map);
-void hmap_add(HMap *map, char *key, void *value, HEType type);
+
+// TODO: doc
+void hmap_add(HMap *map, char *key, void *value, HEType type, uint32_t value_size);
+
+// TODO: doc
 HEntry *hmap_get(HMap *map, char *key);
 
+// TODO: doc
+void hmap_remove(HMap *map, char *key);
+
+/**
+ * @brief Print all hashmap entries
+ *
+ * Print all key values
+ *
+ * @param[in] map
+ */
+void hmap_print_all(HMap *map);
+
 // .c
+
+/** @copydoc hmap_create */
 HMap *hmap_create(size_t capacity)
 {
     if (capacity <= 0)
@@ -66,13 +90,15 @@ HMap *hmap_create(size_t capacity)
     return map;
 }
 
-void hentry_destroy(HEntry *entry)
+// TODO: doc
+static void hentry_destroy(HEntry *entry)
 {
     if (entry == NULL)
         return;
     free(entry);
 }
 
+/** @copydoc hmap_destroy */
 void hmap_destroy(HMap *map)
 {
     if (map == NULL)
@@ -115,9 +141,12 @@ static size_t hmap_build_idx(char *key, size_t capacity)
         acc += *pos;
         pos++;
     }
-    return acc % capacity;
+    size_t idx = acc % capacity;
+    // printf("[DEBUG] key %s index %ld\n", key, idx);
+    return idx;
 }
 
+// TODO: doc
 static HEntry *hmap_get_first(HMap *map, char *key, size_t *out_idx)
 {
     size_t idx = hmap_build_idx(key, map->capacity);
@@ -125,6 +154,7 @@ static HEntry *hmap_get_first(HMap *map, char *key, size_t *out_idx)
     return map->entries[idx];
 }
 
+/** @copydoc hmap_get */
 HEntry *hmap_get(HMap *map, char *key)
 {
     size_t idx = hmap_build_idx(key, map->capacity);
@@ -149,6 +179,7 @@ HEntry *hmap_get(HMap *map, char *key)
     return cur;
 }
 
+// TODO: doc
 static HEntry *hmap_get_by_idx(HMap *map, size_t idx, int *needs_grow)
 {
     if (idx >= map->capacity)
@@ -160,6 +191,7 @@ static HEntry *hmap_get_by_idx(HMap *map, size_t idx, int *needs_grow)
     return map->entries[idx];
 }
 
+// TODO: doc
 static size_t hmap_grow(HMap *map)
 {
     // if capacity if full then resize
@@ -189,10 +221,14 @@ static size_t hmap_grow(HMap *map)
 
     map->entries = new_entries;   // assign new ones
     map->capacity = new_capacity; // with new capacity
+
+    // printf("[DEBUG] new capacity is %ld\n", new_capacity);
+
     return map->capacity;
 }
 
-void hmap_add(HMap *map, char *key, void *value, HEType type)
+/** @copydoc hmap_add */
+void hmap_add(HMap *map, char *key, void *value, HEType type, uint32_t value_size)
 {
     if (map->capacity == map->len)
     {
@@ -247,33 +283,69 @@ void hmap_add(HMap *map, char *key, void *value, HEType type)
     entry->key = key;
     entry->value = value;
     entry->type = type;
+    entry->value_size = value_size;
 
     map->entries[idx] = entry;
     map->len++;
 }
 
+void hmap_print_all(HMap *map)
+{
+    for (size_t ii = 0; ii < map->capacity; ii++)
+    {
+        HEntry *entry = map->entries[ii];
+        if (entry == NULL)
+            continue;
+
+        printf("{ key:%s, value:", entry->key);
+
+        if (entry->type == HE_TYPE_STR)
+        {
+            printf("%s ", (char *)entry->value);
+        }
+        else if (entry->type == HE_TYPE_INT8)
+        {
+            int8_t *value = (int8_t *)entry->value;
+            for (size_t kk = 0; kk < entry->value_size; kk++)
+                printf("%d ", value[kk]);
+        }
+        else if (entry->type == HE_TYPE_INT16)
+        {
+            int16_t *value = (int16_t *)entry->value;
+            for (size_t kk = 0; kk < entry->value_size; kk++)
+                printf("%d ", value[kk]);
+        }
+        else if (entry->type == HE_TYPE_INT32)
+        {
+            int32_t *value = (int32_t *)entry->value;
+            for (size_t kk = 0; kk < entry->value_size; kk++)
+                printf("%d ", value[kk]);
+        }
+        else if (entry->type == HE_TYPE_INT64)
+        {
+            int64_t *value = (int64_t *)entry->value;
+            for (size_t kk = 0; kk < entry->value_size; kk++)
+                printf("%ld ", value[kk]);
+        }
+        printf("}\n");
+    }
+}
+
 // gcc -Wall -Wextra -Wpedantic -O2 -g -std=c99 hmap.c
 int main(void)
 {
-    {
-        HMap *map = hmap_create(2);
-        hmap_add(map, "a", "pluto", HE_TYPE_STR);
-        hmap_add(map, "c", "paperino", HE_TYPE_STR);
-        hmap_add(map, "b", "xyz", HE_TYPE_STR);
+    HMap *map = hmap_create(2);
+    hmap_add(map, "a", "pluto", HE_TYPE_STR, 1);
+    hmap_add(map, "c", "paperino", HE_TYPE_STR, 1);
+    hmap_add(map, "b", "xyz", HE_TYPE_STR, 1);
 
-        HEntry *entry = hmap_get(map, "a");
-        if (entry != NULL)
-            printf("key %s value %s type %d\n", entry->key, (char *)entry->value, entry->type);
+    int8_t x = 112;
+    hmap_add(map, "d", &x, HE_TYPE_INT8, 1);
 
-        entry = hmap_get(map, "b");
-        if (entry != NULL)
-            printf("key %s value %s type %d\n", entry->key, (char *)entry->value, entry->type);
+    int y[] = {1230, 9920, 84530};
+    hmap_add(map, "e", &y, HE_TYPE_INT32, 3);
 
-        entry = hmap_get(map, "c");
-        if (entry != NULL)
-            printf("key %s value %s type %d\n", entry->key, (char *)entry->value, entry->type);
-
-        hmap_destroy(map);
-    }
+    hmap_print_all(map);
+    hmap_destroy(map);
     return 0;
 }
