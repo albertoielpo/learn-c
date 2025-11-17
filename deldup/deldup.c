25447d1bf8868ef55fb9e5efad2df84b10e581e1
Alberto Ielpo <alberto.ielpo@gmail.com>
2025-11-17 10:59:24 +0100

    Third party hast table dismission

--- deldup/deldup.c
+++ deldup/deldup.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../utils/sha1.h"
-#include "../third-party/hash-table/ht.h"
+#include "../utils/hmap.h"
 
 /**
  * Delete a file from the fs
@@ -55,49 +55,50 @@ static bool delete_file(const char *filename)
  */
 static void remove_duplicates(Fhash *fhs, size_t len)
 {
-    // create an hash table to reduce scan complexity
-    ht *table = ht_create(1024);
-    if (table == NULL)
+    // create an hash map to reduce scan complexity
+    HMap *map = hmap_create(1024);
+    if (map == NULL)
     {
-        perror("Cannot allocate hash table");
+        perror("Cannot allocate hash map");
         exit(1);
     }
 
     // init to all zeros
     uint8_t init[20] = {0};
 
+    // convertion from uint8_t to hex string
+    // malloc is used because data are not owned by hash map
+    // so we need to allocate 40 + \0 byte for each hash
+    void *all_hash_str = malloc(sizeof(char) * SHA1_LENGTH_CHAR * len); // [hash_str (41) | hash_str | ....]
+    // point to first
+    char *hash_str = all_hash_str;
+
     for (size_t ii = 0; ii < len; ii++)
     {
-        // convertion from uint8_t to hex string
-        char hash_str[SHA1_LENGTH_CHAR]; // size needed 40 + \0
-
         if (memcmp(fhs[ii].hash, init, SHA1_LENGTH) == 0)
-        {
-            // hash not calculated, all zeros
-            continue;
-        }
+            continue; // hash not calculated, all zeros
 
         // Convert binary hash to hex string
         if (!hash_to_hex(fhs[ii].hash, SHA1_LENGTH, hash_str, SHA1_LENGTH_CHAR))
-        {
-            // cannot continue
-            exit(1);
-        }
+            exit(1); // cannot continue
 
-        void *value = ht_get(table, hash_str);
-        if (value == NULL)
+        HEntry *entry = hmap_get(map, hash_str); // ht_get(map, hash_str);
+        if (entry == NULL)
         {
-            // not found
-            ht_set(table, hash_str, fhs[ii].filename);
+            // not found then HMap *map, char *key, void *value, HEType type, uint32_t value_size
+            hmap_add(map, hash_str, fhs[ii].filename, HE_TYPE_STR, 1);
         }
         else
         {
-            // if found then it's a duplicate
-            printf("%s is a duplicate of %s\n", fhs[ii].filename, ((char *)value));
+            // if found then it's a duplicate. cast to char* is safe
+            printf("%s is a duplicate of %s\n", fhs[ii].filename, ((char *)entry->value));
             delete_file(fhs[ii].filename);
         }
+        hash_str += SHA1_LENGTH_CHAR; // go to the next one
     }
-    ht_destroy(table);
+
+    free(all_hash_str); // destroy hashes
+    hmap_destroy(map);  // destroy map
 }
 
 /**
